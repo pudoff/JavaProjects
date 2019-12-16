b5671ec2cf61e60d842f46ea84dba418df034c35
pudoff <58526967+pudoff@users.noreply.github.com>
2019-12-16 17:16:53 +0500

    Lesson4

--- Main.java
+++ Main.java
@@ -1,58 +1,200 @@
-package com.geekbrainsPudoff.Lesson_3;
+﻿package com.geekbrainsPudoff.lesson_4;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
-/*
-1. Написать программу, которая загадывает случайное число от 0 до 9,
-и пользователю дается 3 попытки угадать это число. При каждой попытке компьютер должен сообщить больше ли указанное пользователем число чем загаданное, или меньше.
-После победы или проигрыша выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
- */
-
 public class Main {
-    public static Scanner scan1 = new Scanner(System.in);
+/*
+Урок 4. Крестики-нолики в процедурном стиле
+1. Полностью разобраться с кодом, попробовать переписать с нуля, стараясь не подглядывать в методичку;
+2. Переделать проверку победы, чтобы она не была реализована просто набором условий, например, с использованием циклов.
+3. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4. Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
+4. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
+    
+ */
+    static int Axis_X = 5;
+    static int Axis_Y = 5;
+    static char field[][] = new char[Axis_X][Axis_Y] ;
 
-    static int getFromScanner(String s, int vMin, int vMax) {
-        int vRetOth;
+    public static final char DOT_EMPTY = '.';
+    public static final char DOT_X = 'X';
+    public static final char DOT_0 = '0';
+    static Scanner Sc= new Scanner(System.in);
+    static Random rand = new Random();
+
+    public static  void main (String[] args) {
+
+     initField ();
+     printField();
+
+     while (true) {
+         plrStep();
+         printField();
+         if (checkWin(DOT_X)) {
+             System.out.println("Победил Игрок!");
+             break;
+         }
+         if (checkDraw()) {
+             System.out.println("Ничья!!!");
+             break;
+         }
+         
+         aiStep();
+         printField();
+         if (checkWin(DOT_0)) {
+             System.out.println("Победил компьютер!");
+             break;
+         }
+         if (checkDraw()) {
+             System.out.println("Ничья!!!");
+             break;
+         }
+     }
+
+      }
+
+       static  void initField () {
+        for (int i = 0; i < Axis_X; i++) {
+            for (int j = 0; j < Axis_Y; j++) {
+                field[i][j] = DOT_EMPTY;
+            }
+        }
+    }
+
+     static  void printField () {
+        for (int i = 0; i < Axis_Y; i++) {
+            if (i == 0) {
+                System.out.print("     " + (i + 1) + " ");
+            } else {
+                System.out.print((i + 1) + " ");
+            }
+        }
+        System.out.println();
+            for (int i = 0; i < Axis_Y; i++) {
+                System.out.print((i+1) + "   |");
+                for (int j = 0; j < Axis_X; j++) {
+                    System.out.print(field[i][j] + "|");
+                }
+                System.out.println();
+            }
+            System.out.println("");
+
+    }
+
+    static void plrStep () {
+        int x;
+        int y;
         do {
-            System.out.println(s);
-            vRetOth = scan1.nextInt();
-        } while (vRetOth < vMin || vRetOth > vMax);
-        return vRetOth;
+            System.out.println("Введите координаты X и Y для установки своей фишки " + DOT_X);
+            x = Sc.nextInt() - 1;
+            y = Sc.nextInt() - 1;
+        }
+        while (!valid(y,x));
+        setSymbol(y,x, DOT_X);
+
     }
 
-    private static int mRandom() {
-        Random vRand = new Random();
-        return vRand.nextInt(10);
+    static void setSymbol (int y, int x, char typeDot) {
+        field[y][x] = typeDot;
     }
-
-    public static void main (String [] args) {
-
-    int vAgain = 1;
+    static void aiStep () {    
+        int x;
+        int y;
        do {
-           int vRand = mRandom(); // загадываем случайное число
-           int vCntTries = 3; // 3 попытки
-
-           for (int i = 1; i <= vCntTries; i++) {
-               int vUserAnswer = getFromScanner("Загадано случайное число от 0 до 9. Попробуйте угадать.", 0, 9);
-               if (vUserAnswer == vRand) {
-                   vAgain  = getFromScanner("Вы угадали ! Повторить игру еще раз? 1 – да / 0 – нет ", 0, 1);
-                   if (vAgain == 0) { break; }
-               } else if (vUserAnswer > vRand & (vCntTries - i) >0) {
-                   System.out.println("Вы не угадали ! Загаданное число меньше вашего.  У вас осталось " + (vCntTries - i) + " попыток");
-               } else if (vUserAnswer < vRand & (vCntTries - i) >0) {
-                   System.out.println("Вы не угадали ! Загаданное число больше вашего.  У вас осталось " + (vCntTries - i) + " попыток");
-               } else if (vUserAnswer != vRand & (vCntTries - i) ==0 )  {
-                   vAgain  = getFromScanner("Вы проиграли ! Повторить игру еще раз? 1 – да / 0 – нет ", 0, 1);
-                   if (vAgain == 0) { break; }
-               }
-           }
-
-       }  while (vAgain == 1);
-        scan1.close();
-        System.out.println("Спасибо за игру !");
+//           int[] vBlock = checkBlock (DOT_0, DOT_X);
+       //    if (vBlock[0] == 0 && vBlock.length == 1) {
+            x = rand.nextInt(Axis_X);
+            y = rand.nextInt(Axis_Y);
+    /*    } else {
+               //x = Sc.nextInt() - 1;
+            x = vBlock[0];
+            y = vBlock[1]; 
+          }*/
+        }
+        while (!valid(y,x));
+        setSymbol(y,x, DOT_0);
     }
 
+    static boolean valid(int y, int x) {
+         
+        if(x < 0 || y < 0 || x > Axis_X-1 || y >Axis_Y - 1)  {
+            return false;
+        }
+        return field[y][x] == DOT_EMPTY;
+    }
 
+    static boolean checkDraw () {
+        for (int i = 0; i < Axis_Y; i++) {
+            for (int j = 0; j < Axis_X; j++) {
+                if (field[i][j]==DOT_EMPTY)   return false;
+            }
+        }
+        return true;
+        
+    }
 
+    static boolean checkWin (char chip) {
+        int sumChips = 0;
+        for (int i = 0; i < Axis_X; i++) {
+            for (int j = 0; j < Axis_Y; j++) {
+                 if (field[i][j] == chip) sumChips++;
+            }
+            if (sumChips == Axis_X) return true;
+            sumChips = 0;
+        }
+
+        for (int i = 0; i < Axis_Y; i++) {
+            for (int j = 0; j < Axis_X; j++) {
+                 if (field[j][i] == chip) sumChips++;
+            }
+            if (sumChips == Axis_Y) return true;
+            sumChips = 0;
+        }
+
+        for (int i = 0; i < Axis_X; i++) {
+            if (field[i][i] == chip) sumChips++;
+        }
+        if (sumChips == Axis_X) return true;
+        sumChips = 0;
+        
+        int j = Axis_X-1;
+        for (int i = 0; i < Axis_X; i++) {
+            if (field[i][j] == chip) sumChips++;
+            j--;
+        }
+        if (sumChips == Axis_X) return true;
+        sumChips = 0;
+
+        return false; 
+    }
+/*
+   static int[] checkBlock( char chipAi, char chipPlr) {
+        int [] vRes = new int[2];
+        int vSumChipsAi = 0;
+        int vSumChipsPlrs = 0;
+        
+        for (int i = 0; i <Axis_X; i++) {
+             for (int j = 0; j < Axis_Y; j++) {
+                  if (field[i][j] == chipAi) {
+                      vSumChipsAi++;
+                } else if (field[i][j] == chipPlr) {
+                          vSumChipsPlrs++;
+                }   
+                if (vSumChipsAi==0 && vSumChipsPlrs>0) {
+                    vRes[0]=i;                    
+                    vRes[1]=j;                                        
+                    return vRes;
+                    
+                }  
+                vSumChipsPlrs =0;
+                vSumChipsAi = 0;
+        } 
+           vSumChipsPlrs = 0; 
+           vSumChipsAi = 0;
+}
+         return vRes;
+} 
+ */
+       
 }
